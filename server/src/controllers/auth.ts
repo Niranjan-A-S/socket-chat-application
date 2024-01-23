9cf4e820251458d5999dd0796be0f5e97638df5d
Niranjan-A-S <niranjan0881@gmail.com>
2024-01-23 15:22:35 +0530

    feat: temp commit

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,8 +1,7 @@
 import { RequestHandler } from 'express';
 
 export const registerUser: RequestHandler = async (req, res) => {
-    console.log(req.body);
-    return res.send('Register User');
+    const { username, email, password } = req.body;
 };
 
 export const loginUser: RequestHandler = async (req, res) => res.send('Login User');
